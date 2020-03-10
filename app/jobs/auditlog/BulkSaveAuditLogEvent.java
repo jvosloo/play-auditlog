8e22c0a0513c74e80f9cb7b1073f255c67d24d69
Johan Vosloo <johan@headspacetech.com>
2020-03-10 20:33:54 +0100

    Refactored BulkSAveAuditEvent job to accept a list of old values

--- app/jobs/auditlog/BulkSaveAuditLogEvent.java
+++ app/jobs/auditlog/BulkSaveAuditLogEvent.java
@@ -1,5 +1,7 @@
 package jobs.auditlog;
 
+import java.util.List;
+
 import models.auditlog.AuditLogEvent;
 import play.jobs.Job;
 import play.modules.auditlog.Auditable.Operation;
@@ -7,16 +9,17 @@ import play.modules.auditlog.Auditable.Operation;
 public class BulkSaveAuditLogEvent extends Job {
 
     private String model;
-    private Long[] modelIds;
+    private List<Long> modelIds;
     private Operation operation;
     private String property;
     private String oldValue;
+    private List<String> oldValues;
     private String newValue;
     private String actor;
     private Long accountId;
     private Long userId;
 
-    public BulkSaveAuditLogEvent(String model, Long[] modelIds, Operation operation, String property, String oldValue, String newValue, String actor, Long accountId, Long userId) {
+    public BulkSaveAuditLogEvent(String model, List<Long> modelIds, Operation operation, String property, String oldValue, String newValue, String actor, Long accountId, Long userId) {
         this.model = model;
         this.modelIds = modelIds;
         this.operation = operation;
@@ -27,13 +30,25 @@ public class BulkSaveAuditLogEvent extends Job {
         this.userId = userId;
         this.accountId = accountId;
     }
+    
+    public BulkSaveAuditLogEvent(String model, List<Long> modelIds, Operation operation, String property, List<String> oldValues, String newValue, String actor, Long accountId, Long userId) {
+        this.model = model;
+        this.modelIds = modelIds;
+        this.operation = operation;
+        this.property = property;
+        this.oldValues = oldValues;
+        this.newValue = newValue;
+        this.actor = actor;
+        this.userId = userId;
+        this.accountId = accountId;
+    }    
 
     @Override
     public void doJob() {
     	
-    	for (Long modelId : this.modelIds) {
-	        new AuditLogEvent(accountId, userId, actor, model, modelId, operation, property, oldValue, newValue).save();
-    	}
+        int n = 0;
+    	for (Long modelId : this.modelIds)
+	        new AuditLogEvent(accountId, userId, actor, model, modelId, operation, property, (oldValues == null ? oldValue : oldValues.get(n++)), newValue).save();
     }
 
 }
